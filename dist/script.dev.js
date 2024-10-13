116f2c0c63d369b4006a24ce5f5caad8c0c6b771
gigg88 <popp.christian88@gmail.com>
2024-10-13 20:56:15 +0300

    adding slider

---
+++ dist/script.dev.js
@@ -0,0 +1,5 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.latest_news_slider').slick({});
+});
---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.latest_news_slider').slick({
+      
+    });
+  });
