be902dd6fc681e2835ee4f384dec37cf8e01e434
gigg88 <popp.christian88@gmail.com>
2024-10-17 10:31:25 +0300

    working on slider

--- script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('.latest_news_slider').slick({
-      
-    });
-  });
---
+++ slider.js
@@ -0,0 +1,11 @@
+$(function(){
+   
+$('.latest_news_slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots:true,
+  prevArrow: '<button class="prev-arrow">&#10096;</button>',
+  nextArrow: '<button class="next-arrow">&#10097;</button>'
+});
+  });
