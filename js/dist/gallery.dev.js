41495d493d6250caffe25285a7ffae4f2493fc24
gigg88 <popp.christian88@gmail.com>
2024-10-18 20:03:56 +0300

    galler

--- js/dist/gallery.dev.js
+++ js/dist/gallery.dev.js
@@ -2,10 +2,13 @@
 
 var _lightgallery = _interopRequireDefault(require("lightgallery"));
 
+var _thumbnail = _interopRequireDefault(require("lightgallery/plugins/thumbnail"));
+
+var _zoom = _interopRequireDefault(require("lightgallery/plugins/zoom"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 (0, _lightgallery["default"])(document.getElementById('lightgallery'), {
   licenseKey: '0000-0000-000-0000',
-  speed: 500 // ... other settings
-
+  speed: 500
 });
--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,12 @@
 import lightGallery from 'lightgallery';
-
+import lgThumbnail from 'lightgallery/plugins/thumbnail'
+import lgZoom from 'lightgallery/plugins/zoom'
 
 
    
    lightGallery(document.getElementById('lightgallery'), {
         licenseKey: '0000-0000-000-0000',
         speed: 500,
-        // ... other settings
+        
     });
 
