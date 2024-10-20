dd767102a5499fd8ee6e9b2a0299cee54351d96f
gigg88 <popp.christian88@gmail.com>
2024-10-20 17:31:43 +0300

    x

--- js/dist/main.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-document.addEventListener('DOMContentLoaded', function () {
-  lightGallery(document.getElementById('lightgallery'), {
-    plugins: [lgZoom, lgThumbnail],
-    licenseKey: 'your_license_key',
-    speed: 500
-  });
-});
