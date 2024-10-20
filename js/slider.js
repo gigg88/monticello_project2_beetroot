842dff50fa48b323d1b3a6e0b91c974ecc3c4d77
gigg88 <popp.christian88@gmail.com>
2024-10-20 20:05:30 +0300

    slider

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,11 @@
-$(function(){
-   
-$('.latest_news_slider').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  dots:true,
-  prevArrow: '<button class="prev-arrow">&#10096;</button>',
-  nextArrow: '<button class="next-arrow">&#10097;</button>'
-});
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+    $('.latest_news_slider').slick({
+      dots: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="nav_button slick-prev"><</button>',
+      nextArrow: '<button type="button" class="nav_button slick-next">></button>'
+    });
   });
